6306359910ebe0d5416494ab87c8b9d4da18efce
Fuad Abou Heit <f.abouheit@gmail.com>
2023-09-22 15:29:05 +0300

    step 5

--- index.js
+++ index.js
@@ -31,4 +31,34 @@ app.get('/search?s=:Search?', (req,res)=>{
    
 })
 
+
+app.get('/movies/create', (req,res)=>{
+
+})
+
+app.get('/movies/read', (req,res)=>{
+    let displayMovies =[]
+     movies.forEach(element => {
+    displayMovies.push(element.title)    
+    });
+    // displayMovies.trim()
+    res.status(200).json({status:200, data: displayMovies })
+})
+
+app.get('/movies/update', (req,res)=>{
+    
+})
+
+app.get('/movies/delete', (req,res)=>{
+    
+})
+
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    // { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
+
 app.listen(3002);
