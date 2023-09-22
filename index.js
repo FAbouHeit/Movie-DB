e83e386dca726e0f0e147274ef514aa6727d546c
Fuad Abou Heit <f.abouheit@gmail.com>
2023-09-22 16:30:19 +0300

    step 4 fixed

--- index.js
+++ index.js
@@ -23,12 +23,21 @@ app.get('/hello/:ID?', (req,res)=>{
     res.json({status: 200, message: `Hello ${req.params.ID}`})
 })
 
-app.get('/search?s=:Search?', (req,res)=>{
-    if(req.params.Search) 
-    res.status(200).json({status:200, message: "ok", data: `${req.params.Search}`})
-    else
-    res.status(500).json({status:500, error: true,message:"you have to provide a search"})
+// app.get('/search?s=:Search?', (req,res)=>{
+//     if(req.params.Search) 
+//     res.status(200).json({status:200, message: "ok", data: `${req.params.Search}`})
+//     else
+//     res.status(500).json({status:500, error: true,message:"you have to provide a search"})
    
+// })
+
+
+app.get('/search', (req,res)=>{
+    // let search = req.query;
+    // console.log(search)
+if(req.query.s) res.status(200).json({status:200, message: "ok", data: `${req.query.s}`})
+else
+    res.status(500).json({status:500, error: true,message:"you have to provide a search"})
 })
 
 
