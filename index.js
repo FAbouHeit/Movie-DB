dfa4fc922fddfc917b87378cd01fad5cdf3bb560
Fuad Abou Heit <f.abouheit@gmail.com>
2023-09-22 12:28:43 +0300

    step 3

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 
+const time = new Date();
+const timeDisplay = `${time.getHours()}:${time.getSeconds()}`
 
 app.get('/', (req,res)=>{
     console.log("here")
-    res.send('OK')
+    
 })
 
-app.listen(3000);
+app.get('/test', (req,res)=>{
+    res.json({status:200, message:"ok"})
+})
+app.get('/time', (req,res)=>{
+    res.json({status:200, message:timeDisplay})
+})
+
+app.listen(3002);
